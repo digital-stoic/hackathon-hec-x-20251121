43d91f9bb70b93d4453eced8418e34313f827b3c
mathieu.francois <mathieu.francois-barseghian@bivwak.com>
2025-11-20 13:29:10 +0100

    Update config with all team names and adjust deadline

--- tools/config.sh
+++ tools/config.sh
@@ -1,8 +1,28 @@
-export DEADLINE="2025-11-21 18:00:00"
+export DEADLINE="2025-11-21 15:00:00"
 
 export TEAMS=(
     "test-mat-1:https://github.com/digital-stoic/simplificator-manager-page"
     "test-mat-2:https://github.com/digital-stoic/simplificator-manager-page"
+    # "team_01_enneagramme_team"
+    # "team_02_lpb"
+    # "team_03_bnp_paribas_team_13"
+    # "team_04_911_v2"
+    # "team_05_los_chechos"
+    # "team_06_les_banquupteurs"
+    # "team_07_supernova"
+    # "team_08_les_prototypeurs"
+    # "team_09_les_skyrocketeurs"
+    # "team_10_bnp_build_new_perspectives"
+    # "team_11_scamm"
+    # "team_12_teamrocket"
+    # "team_13_top_team"
+    # "team_14_les_nullos"
+    # "team_15_albacore"
+    # "team_16_profil_6"
+    # "team_17_les_tigres"
+    # "team_18_hackathletes"
+    # "team_19_choc"
+    # "team_20_cddfgt"
 )
 
 export DRY_RUN=0
