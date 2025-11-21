15cc907447ebd73e05d4e4cf4ef78bfa01abfdd3
mathieu.francois <mathieu.francois-barseghian@bivwak.com>
2025-11-21 14:07:30 +0100

    Aggregate team_04_911 submission
    Source: https://github.com/mathisbaala/bnp-design-hub
    Commit: ab0cf0a99fb70026966fcb3c0b3ed322db664ff2
    Aggregated: 2025-11-21T14:07:28+01:00
    Deadline: 2025-11-21 15:00:00

---
+++ teams/team_04_911/src/App.tsx
@@ -0,0 +1,31 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Philanthropie from "./pages/Philanthropie";
+import SeRenseigner from "./pages/SeRenseigner";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/philanthropie" element={<Philanthropie />} />
+          <Route path="/se-renseigner" element={<SeRenseigner />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
